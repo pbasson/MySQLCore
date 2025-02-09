b5328e66bbe05522df2e97abba108c7238ba2146
preetpal basson <preet.basson@gmail.com>
2025-02-09 15:44:41 +0000

    0.34: Update

--- src/MySQLCore.API/Program.cs
+++ src/MySQLCore.API/Program.cs
@@ -1,7 +1,9 @@
 using MySQLCore.API.Configurations;
+using MySQLCore.Core.CoreHelpers;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.RegisterService(builder.Configuration);
+builder.WebHost.ConfigureKestrel(options => { options.ListenAnyIP(7840, listenOptions => { listenOptions.UseHttps(); }); });
 
 var app = builder.Build();
 app.RegisterApplication();
