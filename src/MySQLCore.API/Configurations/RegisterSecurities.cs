1d2cdf2c6555f0b4b8435e9410d5588c784b297b
preetpal basson <preet.basson@gmail.com>
2025-02-10 11:29:51 +0000

    0.35: Update

---
+++ src/MySQLCore.API/Configurations/RegisterSecurities.cs
@@ -0,0 +1,25 @@
+using System.Security.Cryptography.X509Certificates;
+using MySQLCore.Core.CoreHelpers;
+
+namespace MySQLCore.API.Configurations {
+    public static class RegisterSecurities
+    {
+        public static void RegisterSecurity(this WebApplicationBuilder builder)
+        {
+            var certPath = builder.Configuration[AppSettings.CERTIFICATE_FILE];
+            var certPassword = builder.Configuration[AppSettings.CERTIFICATE_PASSWORD];
+
+            if (!string.IsNullOrEmpty(certPath) && !string.IsNullOrEmpty(certPassword))
+            {
+                var certificate = new X509Certificate2(certPath, certPassword);
+
+                builder.WebHost.ConfigureKestrel(options =>
+                {
+                    options.ListenAnyIP(7840, listenOptions => { listenOptions.UseHttps(certificate); });
+                });
+                Console.WriteLine(API_Variables.CertificateLoaded);
+            }
+            else { Console.WriteLine(API_Variables.CertificateMissing); }
+        }
+    }
+}
--- src/MySQLCore.API/Program.cs
+++ src/MySQLCore.API/Program.cs
@@ -1,9 +1,8 @@
 using MySQLCore.API.Configurations;
-using MySQLCore.Core.CoreHelpers;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.RegisterService(builder.Configuration);
-builder.WebHost.ConfigureKestrel(options => { options.ListenAnyIP(7840, listenOptions => { listenOptions.UseHttps(); }); });
+builder.RegisterSecurity();
 
 var app = builder.Build();
 app.RegisterApplication();
