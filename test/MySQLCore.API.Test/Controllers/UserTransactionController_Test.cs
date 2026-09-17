f8b63876143d69f8e80669e611823a74df8a16c4
Preet Basson <preet.basson@gmail.com>
2026-09-17 11:16:42 +0100

    1.50: Update

--- test/MySQLCore.API.Test/Controllers/UserTransactionController_Test.cs
+++ test/MySQLCore.API.Test/Controllers/UserTransactionController_Test.cs
@@ -1,7 +1,3 @@
-using AutoFixture;
-using MySQLCore.Core.Interfaces.Services.User;
-using MySQLCore.Core.Models.DTOs.User;
-
 namespace MySQLCore.API.Test.Controllers;
 
 public class UserTransactionController_Test : Base_Test
@@ -121,5 +117,123 @@ public class UserTransactionController_Test : Base_Test
         // Assert
         Assert.IsType<BadRequestResult>(result.Result);
         _service.Verify(x => x.GetRecordByIdAsync(It.IsAny<int>(), It.IsAny<CancellationToken>()), Times.Never);
+    }
+
+    [Fact]
+    public async Task CreateRecord_ShouldReturnCreatedAtAction_WhenCreated()
+    {
+        // Arrange
+        var dto = new CreateUserDTO { UserName = "Johndoe", FirstName = "John", LastName = "Doe", Email = "johndoe@example.com"};
+        var response = new TransferDTO(id: 1, message: "Success: Entity Created", serviceResultType: ServiceResultType.Success);
+
+        _service.Setup(x => x.CreateRecordAsync(dto, CancellationToken.None)).ReturnsAsync(response);
+
+        // Act
+        var result = await _controller.CreateRecord(dto, CancellationToken.None);
+
+        // Assert
+        var createdResult = Assert.IsType<CreatedAtActionResult>(result.Result);
+
+        Assert.Equal(StatusCodes.Status201Created, createdResult.StatusCode);
+        Assert.Equal(nameof(_controller.GetRecordById), createdResult.ActionName);
+        Assert.Equal(response, createdResult.Value);
+        Assert.Equal(1, createdResult.RouteValues!["id"]);
+
+        _service.Verify(x => x.CreateRecordAsync(dto, CancellationToken.None), Times.Once);
+    }
+
+    [Fact]
+    public async Task UpdateRecord_ShouldReturnOk_WhenUpdateSucceeds()
+    {
+        // Arrange
+        var dto = new UpdateUserDTO { Id = 1, UserName = "Johndoe", FirstName = "John", LastName = "Doe", Email = "johndoe@example.com"};
+        var response = new TransferDTO(id: 1, message: "Success: Entity Created", serviceResultType: ServiceResultType.Success);
+
+        _service.Setup(x => x.UpdateRecordAsync(dto, CancellationToken.None)).ReturnsAsync(response);
+
+        // Act
+        var result = await _controller.UpdateRecord(dto, CancellationToken.None);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var value = Assert.IsType<TransferDTO>(okResult.Value);
+
+        Assert.Equal(response, value);
+        Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+
+        _service.Verify(x => x.UpdateRecordAsync(dto, CancellationToken.None), Times.Once);
+    }
+
+    [Fact]
+    public async Task UpdateRecord_ShouldReturnNotFound_WhenUpdateFails()
+    {
+        // Arrange
+        var dto = new UpdateUserDTO { Id = 1, UserName = "Johndoe", FirstName = "John", LastName = "Doe", Email = "johndoe@example.com"};
+        var response = new TransferDTO(id: 0, message: "", serviceResultType: ServiceResultType.NotFound);
+
+        _service.Setup(x => x.UpdateRecordAsync(dto, CancellationToken.None)).ReturnsAsync(response);
+
+        // Act
+        var result = await _controller.UpdateRecord(dto, CancellationToken.None);
+
+        // Assert
+        Assert.IsType<NotFoundResult>(result.Result);
+
+        _service.Verify(x => x.UpdateRecordAsync(dto, CancellationToken.None), Times.Once);
+    }
+
+    [Fact]
+    public async Task DeleteRecord_ShouldReturnOk_WhenDeleteSucceeds()
+    {
+        // Arrange
+        var id = 1;
+
+        _service.Setup(x => x.DeleteRecordByIdAsync(id, CancellationToken.None)).ReturnsAsync(true);
+
+        // Act
+        var result = await _controller.DeleteRecord(id, CancellationToken.None);
+
+        // Assert
+        var okResult = Assert.IsType<OkObjectResult>(result.Result);
+        var value = Assert.IsType<bool>(okResult.Value);
+
+        Assert.True(value);
+        Assert.Equal(StatusCodes.Status200OK, okResult.StatusCode);
+
+        _service.Verify(x => x.DeleteRecordByIdAsync(id, CancellationToken.None), Times.Once);
+    }
+
+    [Fact]
+    public async Task DeleteRecord_ShouldReturnOk_WhenDeleteFails()
+    {
+        // Arrange
+        var id = 1;
+
+        _service.Setup(x => x.DeleteRecordByIdAsync(id, CancellationToken.None)).ReturnsAsync(false);
+
+        // Act
+        var result = await _controller.DeleteRecord(id, CancellationToken.None);
+
+        // Assert
+        var resultAssert = Assert.IsType<BadRequestResult>(result.Result);
+
+        Assert.Equal(StatusCodes.Status400BadRequest, resultAssert.StatusCode);
+
+        _service.Verify(x => x.DeleteRecordByIdAsync(id, CancellationToken.None), Times.Once);
+    }
+
+    [Fact]
+    public async Task DeleteRecord_ShouldReturnBadRequest_WhenIdIsZero()
+    {
+        // Arrange
+        var id = 0;
+
+        // Act
+        var result = await _controller.DeleteRecord(id, CancellationToken.None);
+
+        // Assert
+        Assert.IsType<BadRequestResult>(result.Result);
+
+        _service.Verify(x => x.DeleteRecordByIdAsync(It.IsAny<int>(), It.IsAny<CancellationToken>()), Times.Never);
     }
 }
